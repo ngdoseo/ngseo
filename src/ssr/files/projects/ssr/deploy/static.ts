46f6c5ac64b673dfe5995837ccc4e6a30f3fbd39
ampGular <ampgular@gmail.com>
2018-06-02 09:06:45 +0200

    okdk

---
+++ src/ssr/files/projects/ssr/deploy/static.ts
@@ -0,0 +1,27 @@
+import { Observable } from "rxjs";
+import { SpawnOptions, spawn, ChildProcess, exec } from "child_process";
+import {
+    getSystemPath, Path, PathCannotBeFragmentException, normalize,
+  } from '@angular-devkit/core';
+  import { logging, terminal } from '@angular-devkit/core';
+import { join } from "path";
+import { Minimatch } from 'minimatch';
+import * as minimist from 'minimist';
+import { filter } from "rxjs/internal/operators/filter";
+import {clearLine, cursorTo} from 'readline';
+import { exec_child } from "../utils/spwan";
+
+
+
+
+
+
+
+
+
+exec_child("ng",["serve"]).subscribe(X=> 
+  {
+    console.log(X);
+    exec_child("ng",[ "run toh-pt6:server"]).subscribe(p=> console.log('ahora si'))
+
+  });
--- src/ssr/files/projects/ssr/spider-client.ts
+++ src/ssr/files/projects/ssr/spider-client.ts
@@ -29,27 +29,22 @@ enableProdMode();
 
 // Express server
 
-
+import { ROUTES } from "./routes/routes";
 //const ROUTES = ['', '/es/tienda-cosmetica-natural']
 import { createWindow, createDocument } from "domino";
 import { ReplaySubject } from "rxjs";
 import { interval } from 'rxjs';
-import { exec_child, checkIgnoredRoutes, checkEnsuredRoutes, readConfig, configOptions, } from "./utils/spwan";
+import { exec_child } from "./utils/spwan";
 import { map, switchMap } from 'rxjs/operators';
-import { ROUTES } from "./routes/routes";
-
-interface spiderOptions {
-  ignore:string[],
-  whitelist:string[],
-  add:string[],
-}
+import { ChromeRenderer } from "./utils/chrome";
 
 let options = {}
 
-//let config:configOptions = readConfig()
-let spiderConfig:spiderOptions = readConfig().spider;
+let newCrome = new ChromeRenderer();
 
 let route = "";
+//route = ""///es/wellness-y-bienestar-natural/cuidados-de-hammam";
+//const ROUTESX = matcharray(/\/tienda-cosmetica-natural/,ROUTES);
 const SSR_FOLDER = join(process.cwd(), 'projects/ssr');
 const DIST_FOLDER = join(process.cwd(), 'dist/angular.io-example');
 const DIST_FOLDER_SERVER = join(process.cwd(), 'dist/angular.io-example-server');
@@ -71,12 +66,7 @@ const {
 
 const routesSpider: string[] = [];
 const routesDone: string[] = [];
-
 routesSpider.push(route);
-spiderConfig.add.forEach(x=> routesSpider.push(x));
-
-console.log(spiderConfig.add);
-console.log(routesSpider);
 
 let url;
 let i: number = 0;
@@ -85,12 +75,17 @@ const renderURL = new ReplaySubject<string[]>(100);
 
 renderURL.subscribe(
   (options: string[]) => {
-    
+    console.log('he llegado aqui');
         this.url = options[0];
 
-       
+      if (options[1]=='server')
+      {
         renderEachUrl([this.url,options[1]])
-  
+      }
+      else
+      {
+        scrapEachUrl([this.url,options[1]])
+      }
 
   },
   error => {
@@ -125,10 +120,18 @@ function renderEachUrl(url:string[]): void {
           for (let index = 0; index < h1.length; index++) {
             let attr = h1.item(index).getAttribute("href");
 
-       
-            if ( routesDone.indexOf(attr)== -1 && (checkIgnoredRoutes(spiderConfig.ignore,attr) || 
-            checkEnsuredRoutes(spiderConfig.whitelist,attr))
-           )
+
+
+
+            if ( routesDone.indexOf(attr)== -1
+
+            // && exclude
+            // .map(x=>  attr.indexOf(x))
+            // .filter(y=> y==-1).length==3
+              // && attr.indexOf("wellness-y-bienestar-natural-veganos") == -1
+              // && attr.indexOf("/es/regalo-bienestar-spa-gift-vouchers/") == -1
+              // && attr.indexOf("/es/reservas") == -1
+          )
             {
                           routesSpider.push(attr);
             }
@@ -171,9 +174,154 @@ function renderEachUrl(url:string[]): void {
 
 }
 
+function scrapEachUrl(url:string[]): void {
 
 
+
+newCrome.render({ url:"http://localhost:4200/" + url[0]  })
+.then(html => {
+        routesDone.push(url[0]);
+      
+
+        var document = createDocument(html);
+        var body = document.querySelector("body");
+
+        var h1 = body.querySelectorAll("[href]");
+
+        for (let index = 0; index < h1.length; index++) {
+          let attr = h1.item(index).getAttribute("href");
+
+
+
+
+          if ( routesDone.indexOf(attr)== -1
+
+          // && exclude
+          // .map(x=>  attr.indexOf(x))
+          // .filter(y=> y==-1).length==3
+            // && attr.indexOf("wellness-y-bienestar-natural-veganos") == -1
+            // && attr.indexOf("/es/regalo-bienestar-spa-gift-vouchers/") == -1
+            // && attr.indexOf("/es/reservas") == -1
+        )
+          {
+                        routesSpider.push(attr);
+          }
+        }
+
+
+
+        do
+        {
+
+        if (routesDone.indexOf(routesSpider[0])!=-1)
+          {  routesSpider.splice(0,1) }
+
+
+        }
+        while (routesSpider.length!=0 && routesDone.indexOf(routesSpider[0])!=-1
+         );
+
+
+       //   console.log(routesSpider[0].substr(1,2));
+
+        if (routesSpider.length==0)
+        {
+            renderURL.complete();
+
+        }
+        else{
+
+          renderURL.next([routesSpider[0],url[1]])
+        }
+
+
+
+
+
+      })
+      .catch(error => {
+        console.log(error);
+        console.log('soy un error');
+      });
+
+}
+
+
+
+interface Options  {
+  server:boolean,
+  client:boolean,
+ }
+
+const argv = minimist(process.argv.slice(2), {
+  boolean: ['client','server'],
+ });
+
+ const argvOptions = argv;
+
+ let newObject= {}
+ Object.keys(argv).filter(x=> x=='server'|| x=='client').forEach(x=> newObject[x]=argv[x])
+
+
+ if (newObject['server']==true)
+ {
+  console.log('renderind ssr');
   exec_child('ng',['run', 'angular.io-example:server'])
   .subscribe(x=>  renderURL.next([routesSpider[0],'server']) );
+
+
+ }
+
+ else
+ {
+
+  // interval(1000).pipe(
+  //   map(x=>
+  //  request({
+  //    uri: `http://localhost:4200/`,
+  //    })
+  //   .then( y=> {
+  //       return 'ok'})
+  //   .error(err=> {
+
+  //     return 'nook'})
+  //   )
+
+
+  //  ).subscribe(y=> console.log(y))
+ // renderURL.next([routesSpider[0],'client']);
+  exec_child('ng',['build'])
+  .subscribe(x=>  {
+    console.log('Launching Local HOST SERVING THE APP');
+    const app = express();
+
+    const PORT = process.env.PORT || 4200;
+    const DIST_FOLDER = join(process.cwd(), 'dist/angular.io-example');
+    app.get('*.*', express.static(join(DIST_FOLDER)));
+    app.get('*', function(req, res) {
+      res.sendFile(join(DIST_FOLDER,'index.html')) // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node server listening on http://localhost:${PORT}`);
+  newCrome.initialyze()
+  .then(()=> {
+  renderURL.next([routesSpider[0],'client']);
+  })
+  .catch(err=> {console.log('browser not initialyze');
+  console.log(err);
+
+})
+});
+
+
+  })
+
+
+
+ }
+;
 
 
